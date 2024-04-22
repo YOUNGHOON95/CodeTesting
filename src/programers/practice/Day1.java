7fbb159d543e099ae62fdae15a8710ed34911bf7
YOUNGHOON95 <333vnvn333@naver.com>
2024-04-22 23:28:56 +0900

    문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요

---
+++ src/programers/practice/Day1.java
@@ -0,0 +1,27 @@
+package programers.practice;
+
+import java.util.Scanner;
+
+public class Day1 {
+
+	/*
+	 * 
+	 * 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+	 * 1 ≤ str의 길이 ≤ 1,000,000
+	 * str에는 공백이 없으며, 첫째 줄에 한 줄로만 주어집니다..?
+	 * 
+	 * 
+	 * str에 공백이 없다는 말은 입력 받은 값이 공백이 없어도 출력하는 값에는 공백이 없다 라 판단했다 => str에 공백없이 입력해야 한다..?
+	 * 첫째 줄에 한 줄로만 주어진다... 이해를 정확하게는 못했지만 출력값이 한출로만 나온다고 판단
+	 * 
+	 * 조건식 빼먹었다..
+	 * 
+	 */
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+
+}
