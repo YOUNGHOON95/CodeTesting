a6a3b079ec3b99c513641f9d9c07b83e822859b6
YOUNGHOON95 <333vnvn333@naver.com>
2024-08-19 21:30:03 +0900

    feat. 배열의 길이에 따라 다른 연산하기

---
+++ src/programers/practice/Day95.java
@@ -0,0 +1,48 @@
+package programers.practice;
+
+public class Day95 {
+	/*
+	 * 문제 설명
+		정수 배열 arr과 정수 n이 매개변수로 주어집니다. arr의 길이가 홀수라면 arr의 모든 짝수 인덱스 위치에 n을 더한 배열을, 
+		arr의 길이가 짝수라면 arr의 모든 홀수 인덱스 위치에 n을 더한 배열을 return 하는 solution 함수를 작성해 주세요.
+	   
+	   입출력 예
+		arr							n		result
+		[49, 12, 100, 276, 33]		27		[76, 12, 127, 276, 60]
+		[444, 555, 666, 777]		100		[444, 655, 666, 877]
+		
+		
+		입출력 예 #1
+			예제 1번의 arr의 길이는 5로 홀수입니다. 따라서 arr의 짝수 인덱스 0, 2, 4에 주어진 n 값인 27을 더하면 [76, 12, 127, 276, 60]이 됩니다. 따라서 [76, 12, 127, 276, 60]를 return 합니다.
+		입출력 예 #1
+			예제 2번의 arr의 길이는 4로 짝수입니다. 따라서 arr의 홀수 인덱스 1, 3에 주어진 n 값인 100을 더하면 [444, 655, 666, 877]이 됩니다. 따라서 [444, 655, 666, 877]를 return 합니다.
+			
+		=>1.arr길이에 따라 홀 / 짝 구분 2.홀 / 짝 일때 조건 인덱스 값에 +n을 한다.
+	 */
+    public int[] main(int[] arr, int n) {
+        //int[] answer = {};
+        int[] answer = new int[arr.length];
+        //1.arr의 길이 구분 2.홀짝에 따라 index값에 +n 하기
+        if(arr.length % 2 == 0){
+            //짝수
+                for(int i = 0 ; i < arr.length; i++){
+                    if(i % 2 !=0){
+                        answer[i] = arr[i] + n;
+                    }
+                    else{
+                        answer[i] = arr[i];
+                    }
+                }
+        }else{
+            for (int i = 0; i < arr.length; i++) {
+                if (i % 2 == 0) {
+                    answer[i] = arr[i] +n;
+                } else {
+                    answer[i] = arr[i];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
